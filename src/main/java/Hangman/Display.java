635afc04830457f0a0cbafab3c61233e612151f6
Divya-Manivannan1 <reethudya@gmail.com>
2024-04-26 10:04:44 +0100

    printing the word, into msg, and options

--- src/main/java/Hangman/Display.java
+++ src/main/java/Hangman/Display.java
@@ -1,4 +1,29 @@
 package Hangman;
 
 public class Display {
+    public static void printWord(String word, boolean[] guessedRight){
+        for(int i=0; i<guessedRight.length;i++){
+            if(guessedRight[i])
+                System.out.print(word.charAt(i));
+            else
+                System.out.print("_");
+        }
+        System.out.println();
+    }
+    public static void printIntro(){
+        System.out.println("=================================================");
+        System.out.println("<<<<<<<< WELCOME TO DIVYA'S HANGMAN GAME >>>>>>>>");
+        System.out.println("=================================================");
+    }
+    public static void printUserOptions(String[] userOptions){
+        for(byte i=0; i<userOptions.length; i++){
+            System.out.println("Option "+(i+1)+": "+userOptions[i]);
+        }
+    }
+
+    public static void main (String[] args){
+        printIntro();
+        printWord("Hello" , new boolean[]{false, true, false, false, true});
+        printUserOptions(new String[]{"Hi", "hello"});
+    }
 }
