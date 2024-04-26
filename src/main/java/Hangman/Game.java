dd1dbd7d385320b2614b165559b6fff067930a2b
Divya-Manivannan1 <reethudya@gmail.com>
2024-04-26 16:07:22 +0100

    Main menu loop

--- src/main/java/Hangman/Game.java
+++ src/main/java/Hangman/Game.java
@@ -1,4 +1,22 @@
 package Hangman;
 
 public class Game {
+
+
+    public static void main(String[] args) {
+        UserOptions mainMenu = new UserOptions(new String[]{"Play a new Game", "Checkout the rules", "Quit"});
+        byte mainMenuOption = 0;
+        Rules gameRules = new Rules();
+        while (mainMenuOption != 3) {
+            mainMenuOption = mainMenu.getOption();
+            switch (mainMenuOption) {
+                case 1:
+                    System.out.println("Play game");
+                    break;
+                case 2:
+                    gameRules.printRules();
+                    break;
+            }
+        }
+    }
 }
