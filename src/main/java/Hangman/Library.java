d202018f0aab7fe14921e2397287c82a7ac51b06
Divya-Manivannan1 <reethudya@gmail.com>
2024-04-26 09:41:31 +0100

    created the basic classes

---
+++ src/main/java/Hangman/Library.java
@@ -0,0 +1,13 @@
+package Hangman;
+
+import java.util.Random;
+
+public class Library {
+
+    private static final String[] wordBank = new String[]{ "Elvis Presley" , "Taylor Swift" , "Rihanna" , "George Harrison" , "Kendrick Lamar" , "Marlene Dietrich" , "Ciara" , "Shakira"};
+
+    public static String getRandomWord (){
+        Random rand = new Random();
+        return wordBank[rand.nextInt(wordBank.length)];
+    }
+}
