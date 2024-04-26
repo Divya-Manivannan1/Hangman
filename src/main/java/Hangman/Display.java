2645cfe76636391f902d0944ed44ebd35a81432e
Divya-Manivannan1 <reethudya@gmail.com>
2024-04-26 10:40:28 +0100

    removed the main method

--- src/main/java/Hangman/Display.java
+++ src/main/java/Hangman/Display.java
@@ -1,7 +1,7 @@
 package Hangman;
 
 public class Display {
-    public static void printWord(String word, boolean[] guessedRight) {
+    public void printWord(String word, boolean[] guessedRight) {
         for (int i = 0; i < guessedRight.length; i++) {
             if (guessedRight[i])
                 System.out.print(word.charAt(i));
@@ -11,19 +11,19 @@ public class Display {
         System.out.println();
     }
 
-    public static void printIntro() {
+    public void printIntro() {
         System.out.println("=================================================");
         System.out.println("<<<<<<<< WELCOME TO DIVYA'S HANGMAN GAME >>>>>>>>");
         System.out.println("=================================================");
     }
 
-    public static void printUserOptions(String[] userOptions) {
+    public void printUserOptions(String[] userOptions) {
         for (byte i = 0; i < userOptions.length; i++) {
             System.out.println("Option " + (i + 1) + ": " + userOptions[i]);
         }
     }
 
-    public static void printHangman(byte livesLeft) {
+    public void printHangman(byte livesLeft) {
         switch (livesLeft) {
             case 8:
                 System.out.println();
@@ -109,34 +109,14 @@ public class Display {
         System.out.println("Lives left: " + livesLeft);
     }
 
-    public static void printSuccessMessage() {
+    public void printSuccessMessage() {
         System.out.println("Congratulations!! You have found the right word!!");
     }
 
-    public static void printFailureMessage(String word) {
+    public void printFailureMessage(String word) {
         System.out.println("Oh No!! You've run out of lives!!");
         System.out.println("Better luck nest time!!");
         System.out.println("The actual work was " + word);
     }
 
-    public static void main(String[] args) {
-        printIntro();
-        printWord("Hello", new boolean[]{false, true, false, false, true});
-        printUserOptions(new String[]{"Hi", "hello"});
-        printHangman((byte) 0);
-        System.out.println();
-        printHangman((byte) 1);
-        System.out.println();
-        printHangman((byte) 2);
-        System.out.println();
-        printHangman((byte) 3);
-        System.out.println();
-        printHangman((byte) 4);
-        System.out.println();
-        printHangman((byte) 5);
-        System.out.println();
-        printHangman((byte) 6);
-        System.out.println();
-
-    }
 }
