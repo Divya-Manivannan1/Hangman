b98c5b524fce1f843641e52cf537a01bb3ce0136
Divya-Manivannan1 <reethudya@gmail.com>
2024-04-26 11:34:19 +0100

    scanning for a byte with error handling

--- src/main/java/Hangman/UserInput.java
+++ src/main/java/Hangman/UserInput.java
@@ -1,9 +1,30 @@
 package Hangman;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInput {
-    private final Scanner scaner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
-    
+    public static byte getByte(byte max) {
+        byte num = 0;
+        try {
+            num = scanner.nextByte();
+        } catch (InputMismatchException e) {
+            System.out.println("The value you have entered is not a number");
+            scanner.next();
+        }
+        if (num > max || num < 0)
+            num = 0;
+        return num;
+    }
+
+    public static void main(String[] args) {
+        int num;
+        while (true) {
+            num = getByte((byte) 4);
+            System.out.println("no is" + num);
+        }
+    }
+
 }
